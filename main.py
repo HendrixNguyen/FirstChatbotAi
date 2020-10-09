31403e9294f9e2f94df9e2c3455c11d5bdd54f2b
levinguyen <hn112000@gmail.com>
2020-10-09 13:40:06 +0700

    training model

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import tensorflow
 import json
 import random
 
+
 stemmer = LancasterStemmer()
 
 with open("intents.json") as f:
@@ -20,13 +21,13 @@ for intent in data["intents"]:
     for pattern in intent["patterns"]:
         wrds = nltk.word_tokenize(pattern)
         words.extend(wrds)
-        docs_x.append(pattern)
-        doc_y.append(intent["tag"])
+        docs_x.append(wrds)
+        docs_y.append(intent["tag"])
 
     if intent["tag"] not in labels:
         labels.append(intent["tag"])
 
-words = [stemmer.stem(w.lower()) for w in words]
+words = [stemmer.stem(w.lower()) for w in words if w != "?"]
 words = sorted(list(set(words)))
 
 labels = sorted(labels)
@@ -34,9 +35,9 @@ labels = sorted(labels)
 training = []
 output = []
 
-out_emty = [0 for _ in range(len(classes))]
+out_emty = [0 for _ in range(len(labels))]
 
-for x,doc in enumerate(docs_x):
+for x, doc in enumerate(docs_x):
     bag = []
     wrds = [stemmer.stem(w) for w in doc]
 
@@ -47,12 +48,25 @@ for x,doc in enumerate(docs_x):
             bag.append(0)
 
     output_row = out_emty[:]
-    out_row[labels.index(docs_y(docs_y[x]))] = 1
+    output_row[labels.index(docs_y[x])] = 1
 
     training.append(bag)
-    output.append(out_row)
+    output.append(output_row)
 
-training = numpy.array(trainning)    
+training = numpy.array(training)    
 output = numpy.array(output)
+
+tensorflow.reset_default_graph()
+
+net = tflearn.input_data(shape=[None, len(training[0])])
+net =tflearn.fully_connected(net, 8)
+net =tflearn.fully_connected(net, 8)
+net =tflearn.fully_connected(net, len(output[0]), activation="softmax")
+net = tflearn.regression(net)
+
+model = tflearn.DNN(net)
+
+model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
+model.save("model.tflearn")
 #if __name__ == "__main__":
 
